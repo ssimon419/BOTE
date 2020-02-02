ef54be8f817e73886b8f9b983719fdcc85e8c664
justinbroce <58522351+justinbroce@users.noreply.github.com>
2020-02-02 02:54:22 -0500

    Add files via upload

---
+++ cardDraw.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cardDraw : MonoBehaviour
+{
+    // array of all possible units
+    Random random = new Random();
+    String[] units = new string[] { "Archer", "Executioner", "Jester", "Knight", "Mage", "Merchant", "Peasant", "Priest", "Regent", "Squire" };
+    public void cardDraw(Player player) {
+        
+        player.Addcard(units[random.Next(10)]);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+ 
